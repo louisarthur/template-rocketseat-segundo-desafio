adba6726d408350a2f7ac55719fe4df5105a5a52
Louis Arthur Machado Bezerra do Nascimento <louisarthur@ufrn.edu.br>
2021-04-29 14:33:25 -0300

    finalizando a separação de modulos

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,20 @@
-export function Content() {
-  // Complete aqui
+import { GenreResponseProps } from "../@types/genreResponseInterface";
+import { MovieProps } from "../@types/moviePropsInterface";
+import { MovieCard } from "./MovieCard";
+
+
+export const Content = (props: {movies: MovieProps[], selectedGenre: GenreResponseProps}) => { 
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+        </header>
+        <main>
+          <div className="movies-list">
+            {props.movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+    </div>);
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,21 @@
-export function SideBar() {
-  // Complete aqui
+
+import { GenreResponseProps } from "../@types/genreResponseInterface";
+import { Button } from "./Button";
+
+export const SideBar = (props:{genres: GenreResponseProps[],selectedGenreId:number, handleClickButton: Function}) => {
+  return (
+  <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+    <div className="buttons-container">
+      {props.genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => props.handleClickButton(genre.id)}
+          selected={props.selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>);
 }
